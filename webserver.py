c4b03c093d88586555e39f9fa8764244a97c5d93
Abdul <81208506+git-Abdul@users.noreply.github.com>
2023-08-01 18:02:30 +0530

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,29 +1,22 @@
-from flask import Flask
-
+from flask import Flask, send_file
 from threading import Thread
-
-
 
 app = Flask('')
 
 
-
 @app.route('/')
-
 def home():
-
-    return "I'm alive"
-
+    return send_file('index.html')
 
 
 def run():
-
-  app.run(host='0.0.0.0',port=8080)
-
+    app.run(host='0.0.0.0', port=8080)
 
 
-def keep_alive():  
-
+def keep_alive():
     t = Thread(target=run)
-
     t.start()
+
+
+if __name__ == "__main__":
+    keep_alive()
